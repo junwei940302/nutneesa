9e3b08b58933e7b1cac9665d1ef65c0d1244210d
Junwei <henryhuang0302@gmail.com>
2025-06-27 18:13:00 +0800

    go deploy frontend

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,2 +1,2 @@
-const API_URL = 'http://localhost:3001'; //開發環境
-//const API_URL = 'https://nutneesa.onrender.com';//正式部署
+//const API_URL = 'http://localhost:3001'; //開發環境
+const API_URL = 'https://nutneesa.onrender.com';//正式部署
